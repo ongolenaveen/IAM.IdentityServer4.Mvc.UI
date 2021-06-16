6379892f5d3c0a63ee6e2179a080e0ec22e62553
Naveen Papisetty <naveenbabu.papisetty@gmail.com>
2021-06-16 06:55:29 +0100

    Working Code

---
+++ StartupExtensions.cs
@@ -0,0 +1,29 @@
+﻿using IdentityServer.Mvc.UI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net;
+using System.Net.Http;
+
+namespace IdentityServer.Mvc.UI
+{
+    public static class StartupExtensions
+    {
+        public static void ConfigureServices(this IServiceCollection services, IConfiguration config)
+        {
+            //services.AddScoped<ILoginService,LoginService>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            var cookies = new CookieContainer();
+            var handler = new HttpClientHandler
+            {
+                CookieContainer = cookies,
+                UseCookies = true
+            };
+            services.AddHttpClient<ILoginService, LoginService>().ConfigurePrimaryHttpMessageHandler(() =>
+            {
+                return handler;
+            });
+        }
+    }
+}
